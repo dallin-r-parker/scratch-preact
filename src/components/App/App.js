8146226c39f3b1227ca176f24f511a848cf24497
Dallin Parker <dallin.r.parker@gmail.com>
2017-09-22 19:37:04 -0400

    rendering with preact

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import {h, Component} from 'preact'
+
+
+export default class App extends Component {
+
+	render(props, state, context) {
+		return (
+			<div className="main-container">
+				<h1>My name is Dallin</h1>
+			</div>
+		)
+	}
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import {h, render} from 'preact'
+import App from './components/App/App'
+
+render(<App/>, document.getElementById('root'))
